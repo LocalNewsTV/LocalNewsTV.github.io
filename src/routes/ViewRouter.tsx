0ff93efdaa072790e6f4ebb5b3ca0836953a28f3
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-07-18 20:58:11 -0700

    Added stub pages and routing

--- src/routes/ViewRouter.tsx
+++ src/routes/ViewRouter.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Views/Home/Home';
+import About from '../components/Views/About/About';
+import Projects from '../components/Views/Projects/projects';
+import Contact from '../components/Views/Contact/Contact';
+import Resume from '../components/Views/Resume/Resume';
 
 export default function ViewRouter() {
   return (
     <Routes>
       <Route path="/" Component={Home} />
-      <Route path="/About" Component={Home} />
-      <Route path="/Projects" Component={Home} />
-      <Route path="/Contact" Component={Home} />
-      <Route path="/Resume" Component={Home} />
+      <Route path="/About" Component={About} />
+      <Route path="/Projects" Component={Projects} />
+      <Route path="/Contact" Component={Contact} />
+      <Route path="/Resume" Component={Resume} />
       <Route path="*" Component={Home} />
     </Routes>
   )
